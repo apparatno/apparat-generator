47344862f2d72fb165927f521bc2200cdcba1c62
Eirik Årdal <eaardal@outlook.com>
2019-03-09 19:44:52 +0100

    logging

--- src/generator/generator.js
+++ src/generator/generator.js
@@ -4,6 +4,7 @@ import translate from 'google-translate-api';
 import { getRandomInt } from '../utils/randomUtil';
 
 const translateNoun = async (noun) => {
+  console.log(`Translating noun "${noun}"`);
   try {
     const res = await translate(noun, { to: 'no' });
     return res.text;
@@ -14,11 +15,16 @@ const translateNoun = async (noun) => {
 };
 
 const getRandomNoun = () => {
+  console.log('__dirname', __dirname);
   const nounlistPath = path.resolve(__dirname, 'data', 'nounlist.txt');
+  console.log('nounlistPath', nounlistPath);
   const nounsAsString = fs.readFileSync(nounlistPath, { encoding: 'UTF-8' });
+  console.log('nounsAsString', nounsAsString);
   const nouns = nounsAsString.split('\n');
   const randomIndex = getRandomInt(0, nouns.length - 1);
-  return nouns[randomIndex];
+  const n = nouns[randomIndex];
+  console.log('n', n);
+  return n;
 };
 
 const generateApparat = async () => {
